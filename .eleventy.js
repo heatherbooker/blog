dad9c16de7fe07a6cdf72224cc11c731ca72fcdf
hboo <heather.booker@hotmail.com>
2021-02-03 14:34:17 -0500

    Remove /layouts dir

--- .eleventy.js
+++ .eleventy.js
@@ -14,7 +14,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.setDataDeepMerge(true);
 
-  eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
+  eleventyConfig.addLayoutAlias("post", "post.njk");
 
   eleventyConfig.addFilter("readableDate", dateObj => {
     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
